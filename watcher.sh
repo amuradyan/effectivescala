6643de140da1870f616a88c47da772aa9f5db54c
darthspectrum <muradianator@gmail.com>
2019-11-25 22:39:08 +0400

    Updated the watcher script

--- watcher.sh
+++ watcher.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+# YADW: We want to skip one of two identical events from inotifywait.
+# Could be a WSL issue
+CONSUME_EVENT=true
+
 inotifywait -m -e modify --fromfile sources | while read line
 do
-  make clean
-  make all
+  if [ $CONSUME_EVENT = true ]
+  then
+    make all
+    CONSUME_EVENT=false
+  else
+    CONSUME_EVENT=true
+  fi
 done
